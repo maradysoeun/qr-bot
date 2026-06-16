72914e33ae6a3f814f744384b4ff801ae5e890fe
maradysoeun <maradysoeun18@gmail.com>
2026-06-16 16:57:35 +0700

    Update telegram_maps_qr_bot.py

--- telegram_maps_qr_bot.py
+++ telegram_maps_qr_bot.py
@@ -102,7 +102,7 @@ def parse_coordinates(text):
     return None
 
 def generate_qr(url):
-    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=5, border=4)
     qr.add_data(url)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
